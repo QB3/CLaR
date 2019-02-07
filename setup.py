c08afe334d72ba6f50fb6219b902faf56fbc30f7
mathurinm <mathurin.massias@gmail.com>
2019-02-07 10:58:40 +0900

    add version and other info

--- setup.py
+++ setup.py
@@ -1,8 +1,36 @@
+import os
+
 from distutils.core import setup
 
+
+descr = 'Concomitant Lasso with repetitions'
+
+version = None
+with open(os.path.join('clar', '__init__.py'), 'r') as fid:
+    for line in (line.strip() for line in fid):
+        if line.startswith('__version__'):
+            version = line.split('=')[1].strip().strip('\'')
+            break
+if version is None:
+    raise RuntimeError('Could not determine version')
+
+DISTNAME = 'clar'
+DESCRIPTION = descr
+MAINTAINER = 'Quentin Bertrand'
+MAINTAINER_EMAIL = 'quentin.bertrand@inria.fr'
+LICENSE = 'BSD (3-clause)'
+DOWNLOAD_URL = 'https://github.com/QB3/clar.git'
+VERSION = version
+# URL = 'https://qb3.github.io/clar'
+
 setup(name='clar',
-      version='0.1dev',
-      description='CLAR',
-      author='under review',
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=open('README.rst').read(),
+      license=LICENSE,
+      maintainer=MAINTAINER,
+      maintainer_email=MAINTAINER_EMAIL,
+      # url=URL,
+      download_url=DOWNLOAD_URL,
       packages=['clar'],
       )
